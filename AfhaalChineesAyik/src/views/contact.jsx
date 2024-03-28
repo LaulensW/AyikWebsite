9d2fe8715102f9bc1a5543b1b4295b7d3696e625
lautje-69 <61787845+LaulensW@users.noreply.github.com>
2024-03-28 12:03:57 +0100

    Contact form + iframe

--- AfhaalChineesAyik/src/views/contact.jsx
+++ AfhaalChineesAyik/src/views/contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../Components/Layout";
+import ContactPage from "../Components/ContactPage";
 
 const Contact = () => {
     return (
         <div>
             <Layout></Layout>
+            <ContactPage />
         </div>
     );
 }
