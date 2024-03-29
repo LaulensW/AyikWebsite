314d76e630b3d71b755d3f690920650bd1634ef5
lautje-69 <61787845+LaulensW@users.noreply.github.com>
2024-03-29 15:27:34 +0100

    begin popu items

---
+++ AfhaalChineesAyik/src/Components/Popu-items.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Popu-items.css";
+
+const PopuItems = () => {
+  return (
+    <>
+        <h1>Populaire menu's</h1>
+        <p>text</p>
+        <div className="popu-item">
+            
+        </div>
+    </>
+  );
+}
+
+export default PopuItems;
--- AfhaalChineesAyik/src/views/home.jsx
+++ AfhaalChineesAyik/src/views/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from '../Components/Layout';
 import Hero from "../Components/Hero";
+import PopuItems from "../Components/Popu-items";
 import Reviews from "../Components/reviews";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
                 {/* <h1>Home</h1> */}
             </Layout>
             <Hero />
+            <PopuItems />
             <Reviews />
         </>
     );
